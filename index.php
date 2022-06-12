cd5897c1cf0f863fb1f8d67d37cfcc82d76ccc02
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-12 22:27:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php include_once("lib/views/header.php"); ?>
+<body>
+<?php include_once("lib/views/new_nav.php"); ?>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="image/249790.jpg" alt="img1" >
+        <div class="carousel-caption d-none d-md-block">
+            <h5>HI</h5>
+            <p>All</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+    <img src="image/266503.jpg" alt="img1" >
+        <div class="carousel-caption d-none d-md-block">
+            <h5>HI</h5>
+            <p>All</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+    <img src="image/439635.jpg" alt="img1" >
+        <div class="carousel-caption d-none d-md-block">
+            <h5>HI</h5>
+            <p>All</p>
+        </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+
+<div class="home-content">
+    <div class="title">
+        faculties 
+    </div><br>
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="image/266503.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">Faculty of Information Technology</h5>
+      <a href="#"><button class="home-btn">More Infor</button></a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="image/439635.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">Faculty Management</h5>
+        <a href="#"><button class="home-btn">More Infor</button></a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="image/249790.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">Faculty English</h5>
+      <a href="#"><button class="home-btn">More Infor</button></a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="course">
+    <div class="course-title">
+        Courses
+    </div>
+    <br>
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="image/266490.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">BSc in Information Technology</h5>
+
+   </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="image/266490.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">BTech in Information Technology</h5>
+
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="image/266503.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">BSc in Management</h5>
+
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="image/439635.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">BA in English</h5>
+
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="image/249790.jpg" alt="Card image cap" style="height: 300px;">
+    <div class="card-body">
+      <h5 class="card-title">Higher Diploma in Information Technology</h5>
+
+    </div>
+  </div>
+</div>
+</div>
+
+
+<?php include_once("lib/views/footer.php"); ?>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
