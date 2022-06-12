3f0e12c7af13d30a6cb28b2b0ff74290f6825766
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-12 19:10:28 +0530

    Add files via upload

---
+++ complete_campus_system.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2022 at 03:38 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `complete_campus_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_user_tbl`
+--
+
+CREATE TABLE `temp_user_tbl` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `payment` int(10) NOT NULL,
+  `pay_date` date NOT NULL,
+  `tp_no` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_tbl`
+--
+
+CREATE TABLE `user_tbl` (
+  `id` char(30) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `name_with_latters` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nic` varchar(20) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `add1` varchar(255) NOT NULL,
+  `add2` varchar(255) NOT NULL,
+  `add3` varchar(255) NOT NULL,
+  `tp_no` varchar(20) NOT NULL,
+  `roll` varchar(20) NOT NULL,
+  `status` int(1) NOT NULL,
+  `enroll_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `temp_user_tbl`
+--
+ALTER TABLE `temp_user_tbl`
+  ADD PRIMARY KEY (`id`,`email`);
+
+--
+-- Indexes for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  ADD PRIMARY KEY (`id`,`email`,`nic`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `temp_user_tbl`
+--
+ALTER TABLE `temp_user_tbl`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
