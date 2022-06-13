d3f6d8a5529b8c2140553d2d96e6cb9ab27a60e5
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-13 19:47:36 +0530

    Update login_nav.php

--- lib/views/login_nav.php
+++ lib/views/login_nav.php
@@ -2,6 +2,7 @@
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">&nbsp;&nbsp;University Campus</a>
     <span class="navbar-text">
-        <a href="lib/auth/temp_reg.php">New to Campus</a> &nbsp;&nbsp;&nbsp;   
+        <a href="../auth/temp_reg.php">New to Campus</a> &nbsp;&nbsp;&nbsp;
+        <a href="../../index.php"><button class="back-btn">Back</button></a>   
     </span>
 </nav>
