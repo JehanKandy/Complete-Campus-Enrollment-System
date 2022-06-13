3caf525d7319224b8cce8eb50419a278bb1ab777
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-13 17:58:16 +0530

    Update login.php

--- lib/auth/login.php
+++ lib/auth/login.php
@@ -2,6 +2,29 @@
 <body>
 <?php include_once("../views/login_nav.php"); ?>
 
+<div class="login">
+    <div class="login-content">
+        <div class="login-box">
+            <div class="login-title">
+                Login Here
+            </div>
+            <hr>
 
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                <div class="login-label">Username : </div>
+                <input type="text" name="username" id="username" class="login-input" placeholder="Username">
+
+                <div class="login-label">Password : </div>
+                <input type="password" name="password" id="password" class="login-input" placeholder="Password">
+                <br><br>
+                <input type="submit" value="Login" class="login-btn" name="login">
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include_once("../views/footer1.php"); ?>
 </body>
 </html>
