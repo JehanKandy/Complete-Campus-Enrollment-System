546ea5df0cbc09c5fae19bc1c79dd95f891098ff
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-13 19:48:59 +0530

    Add files via upload

---
+++ lib/views/footer1.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="../../css/style.css">
+<div class="footer1">
+    <p>DEVELOPED BY <a href="https://github.com/JehanKandy" target="_blank"> &copy;JEHANKANDY</a> 2022 June</p>    
+</div>
