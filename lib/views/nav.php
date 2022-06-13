3dc07f3f524655c7b37d82c41b52e562ce06b627
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-13 19:48:00 +0530

    Update nav.php

--- lib/views/nav.php
+++ lib/views/nav.php
@@ -2,6 +2,7 @@
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">&nbsp;&nbsp;University Campus</a>
     <span class="navbar-text">
-        Apply as New
+        Apply as New &nbsp;&nbsp;&nbsp;
+        <a href="../../index.php"><button class="back-btn">Back</button></a>
     </span>
 </nav>
