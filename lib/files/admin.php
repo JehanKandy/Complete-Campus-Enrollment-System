43b057a67c643ca86eb2e2fb73e4e10ea718b716
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-14 08:55:30 +0530

    Update admin.php

--- lib/files/admin.php
+++ lib/files/admin.php
@@ -1,1 +1,8 @@
 <?php include_once("../views/no_back.php"); ?>
+<?php include_once("../views/header.php"); ?>
+
+    <frameset cols="18%, *" border="0">
+        <frame name="menu" src="menu.php">
+        <frame name="home" src="" >
+    </frameset>
+</html>   
