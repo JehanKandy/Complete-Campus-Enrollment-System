5a323b94145af5fbcc2652953539729660e42650
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-14 08:56:01 +0530

    Add files via upload

---
+++ lib/files/menu.php
@@ -0,0 +1,42 @@
+<?php include_once("../views/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+<div class="list">
+    <div class="list-content">
+        <div class="list-title">
+        <i class='fas fa-graduation-cap' style='font-size:24px'></i> &nbsp;University Campus
+        </div>
+        <div class="box">
+            <a href="">
+                <div class="list-items">
+                <i class='fas fa-tachometer-alt' style='font-size:20px'></i> &nbsp;Dashboard
+                </div>
+            </a>
+            <a href="">
+                <div class="list-items">
+                <i class='fas fa-user-graduate' style='font-size:20px'></i> &nbsp;Students
+                </div>
+            </a>
+            <a href="">
+                <div class="list-items">
+                <i class='fas fa-chalkboard-teacher' style='font-size:20px'></i> &nbsp;Lecturers
+                </div>
+            </a>
+            <a href="">
+                <div class="list-items">
+                <i class='fas fa-user-tie' style='font-size:20px'></i> &nbsp;Staff
+                </div>
+            </a>
+            <a href="">
+                <div class="list-items">
+                    Admins
+                </div>
+            </a>
+        </div>
+        <hr width="110%">
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
